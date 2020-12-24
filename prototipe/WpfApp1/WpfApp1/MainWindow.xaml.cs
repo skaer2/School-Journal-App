1ccc5940e5ac8ddb3226516ddb396d4b5fc8b37c
OVOSKOP <parodia1234321@gmail.com>
2020-12-24 17:30:14 +0300

    Test prototipe

---
+++ prototipe/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Router.router.setMainFrame(MainFrame);
+
+            Router.router.nextPage(new page.Auth());
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ prototipe/WpfApp1/WpfApp1/Router.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class Router
+    {
+        public static Router router = new Router();
+        private Frame frame = new Frame();
+        private List<Page> pagesHistory = new List<Page>();
+        private Window prevModal = new Window();
+
+        public void setMainFrame(Frame mainFrame) {
+            frame = mainFrame;
+        }
+
+        public void nextPage(Page page) {
+            frame.Navigate(page);
+
+            pagesHistory.Add(page);
+        }
+
+        public void prevPage() {
+            pagesHistory.RemoveAt(pagesHistory.Count - 1);
+            frame.Navigate(pagesHistory[pagesHistory.Count - 1]);
+        }
+
+        public void showModal(Window modal) {
+            prevModal = modal;
+
+            modal.ShowDialog();
+        }
+    }
+}
