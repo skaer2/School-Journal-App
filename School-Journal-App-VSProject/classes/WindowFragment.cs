ea93a2fef37ccff5d0fb9c06cc9ea593b28f166a
skaer2 <max.bog200174@gmail.com>
2020-12-24 21:05:21 +0300

    Made VS project, WindowFragment class, gitignore WindowFragment is the class containing basic window fuctions

---
+++ School-Journal-App-VSProject/classes/WindowFragment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class WindowFragment : Page
+    {
+
+        private List<Tuple<Frame, Page>> framesAndBlocks;
+
+        public WindowFragment()
+        {
+            framesAndBlocks = new List<Tuple<Frame, Page>>();
+        }
+        public void addFrameBlock(Frame frame, Page block)
+        {
+            framesAndBlocks.Add(Tuple.Create<Frame, Page>(frame, block));
+        }
+
+        public void loadBlocks()
+        {
+            foreach (var frAndBl in framesAndBlocks)
+            {
+                loadBlock(frAndBl.Item1, frAndBl.Item2);
+            }
+        }
+
+        private void loadBlock(Frame frame, Page block)
+        {
+            frame.Navigate(block);
+        }
+
+        }
+}
---
+++ School-Journal-App-VSProject/pages/WindowFragment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    // usage: add an object of this class to a page(window) class, 
+    //        then with the "addFrameBlock" function add each frame and a block for it.
+    //        Use "loadBlocks" to load all added blocks in the page
+    class WindowFragment 
+    {
+
+        private List<Tuple<Frame, Page>> framesAndBlocks;
+
+        public WindowFragment()
+        {
+            framesAndBlocks = new List<Tuple<Frame, Page>>();
+        }
+        public void addFrameBlock(Frame frame, Page block)
+        {
+            framesAndBlocks.Add(Tuple.Create<Frame, Page>(frame, block));
+        }
+
+        public void loadBlocks()
+        {
+            foreach (var frAndBl in framesAndBlocks)
+            {
+                loadBlock(frAndBl.Item1, frAndBl.Item2);
+            }
+        }
+
+        private void loadBlock(Frame frame, Page block)
+        {
+            frame.Navigate(block);
+        }
+
+        }
+}
